d02ed7de8ab23df1dda8b9fc7f9d3c9a599c7132
Sydletskyi <sydletsky_petro@ukr.net>
2024-04-24 20:51:49 +0300

    sql storage

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,40 @@
 package config
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type Config struct {
-	Env string `yaml: "environment" env: "ENV" envDefault: "development"`
+	Env         string `yaml: "env" env: "ENV" env-default: "local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string
+	Timeout     time.Duration
+	IdleTimeout time.Duration
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist((err)) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	return &cfg
 }
